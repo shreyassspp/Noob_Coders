acde563f3b8a5d6a3018bbe4cbd627cd6effc3d5
dheer <dheer.samtani@gmail.com>
2021-09-25 19:31:55 +0530

    changes to temp

--- app/src/main/java/com/example/noob_coders/MainActivity.java
+++ app/src/main/java/com/example/noob_coders/MainActivity.java
@@ -1,25 +1,75 @@
 package com.example.noob_coders;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApiNotAvailableException;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    Button mLogOutButton;
+    private DrawerLayout drawer;
+    // Button mLogOutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLogOutButton = findViewById(R.id.logout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new CounsellingFragment()).commit();
+
+            navigationView.setCheckedItem(R.id.nav_counselling);}
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_counselling:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CounsellingFragment()).commit();
+                break;
+
+            case R.id.nav_community:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CommunityFragment()).commit();
+                break;
+
+            case R.id.nav_projects:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProjectsFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;}
+
+       /** mLogOutButton = findViewById(R.id.logout);
 
         mLogOutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +79,15 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+    } **/
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 
