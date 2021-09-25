7b1347008188db6d3a55527ce59449e325823f09
ShreyasPakhare <shreyas_pp@ee.iitr.ac.in>
2021-09-25 15:17:30 +0530

    Changes Added

--- app/src/main/java/com/example/noob_coders/MainActivity.java
+++ app/src/main/java/com/example/noob_coders/MainActivity.java
@@ -2,13 +2,7 @@ package com.example.noob_coders;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
 }
