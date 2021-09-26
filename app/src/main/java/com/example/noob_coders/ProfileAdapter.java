7636c82e2c0c8a6b7930ac749e3a0e9c47ecfdee
ShreyasPakhare <shreyas_pp@ee.iitr.ac.in>
2021-09-26 11:22:44 +0530

    Community Beta

---
+++ app/src/main/java/com/example/noob_coders/ProfileAdapter.java
@@ -0,0 +1,54 @@
+package com.example.noob_coders;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProfileAdapter extends ArrayAdapter<Profile> {
+
+
+    public ProfileAdapter(Activity context, List<Profile> profiles) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, 0, profiles);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Check if the existing view is being reused, otherwise inflate the view
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+        }
+
+        // Get the {@link AndroidFlavor} object located at this position in the list
+        Profile current_profile = getItem(position);
+
+        TextView field_name = (TextView) listItemView.findViewById(R.id.field_user);
+
+        TextView user_email = (TextView) listItemView.findViewById(R.id.email_id_user);
+
+        field_name.setText(current_profile.getField());
+        user_email.setText(current_profile.getEmail());
+
+
+        return listItemView;
+    }
+}
+
